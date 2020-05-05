a0c52bd1c1becc827e364bdac562420dbb69987a
Denzell Grant <denzgrant@gmail.com>
2020-05-04 19:53:01 -0700

    created objects for company roles

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,17 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee"); 
+
+class Engineer extends Employee {
+    constructor(gitHubUser) {
+        super(name, id, email)
+        this.gitHubUser = gitHubUser;
+    }
+    getRole() {
+        return Engineer; 
+    }
+    getGitHub() {
+        return this.gitHubUser; 
+    }
+}
+
+module.exports = Engineer; 
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(officeNum) {
+        super(name, id, email);
+        this.officeNum = officeNum;
+    };
+    getRole() {
+        return Manager;
+    };
+    getOffice() {
+        return this.officeNum;
+    };
+}
+module.exports = Manager; 
